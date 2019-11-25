a6569e4a48e91077515f1e3124bdb9ff8c2a8926
Rosa Richter <cosmic.lady.rosa@gmail.com>
2019-11-25 00:33:34 -0600

    Bump version to 1.4.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule IntSet.MixProject do
   def project do
     [
       app: :int_set,
-      version: "1.4.1",
+      version: "1.4.2",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: "A time- and memory-efficient unordered data structure for positive integers.",
